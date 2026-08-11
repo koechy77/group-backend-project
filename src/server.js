cf85555b489251703ce0f5bd4bd0576e88c041f4
koechy77 <kingsleyonyem7@gmail.com>
2026-08-11 12:47:06 +0100

    required app module, and started the server

---
+++ src/server.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+const app = require("./app");
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+})
